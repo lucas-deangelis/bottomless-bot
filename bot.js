25d4910ca05d0eadf7e855a90ca95e40047490c9
Lucas De Angelis <lucas.de-angelis@epitech.eu>
2019-10-25 18:51:56 +0200

    It works!

--- bot.js
+++ bot.js
@@ -1,24 +1,24 @@
-var Discord = require('discord.io');
-var logger = require('winston');
 require('dotenv').config();
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
-});
+
 
 // Dirty implementation but I have to go fast
-const beginning = new Date('October 1, 2019');
+const beginning = new Date('October 1, 2019 15:17:00');
 const oneDay = 24 * 60 * 60 * 1000;
 function diffDays(firstDate, secondDate) {
-    return Math.round(Math.abs((firstDate - secondDate) / oneDay));
+    let diff = firstDate.getTime() - secondDate;
+    let days = diff/oneDay;
+    let absDays = Math.abs(days);
+    let roundDays = Math.round(absDays);
+    return roundDays;
 }
+
 /*
 Total is 83 :
 13 * 5 = 65 episodes
 4 concerts
 4 * 3 + 2 = 14 zesshoushinai
  */
+
 var episodes = [
     "S1 E1", "S1 E2", "S1 E3", "S1 E4", "S1 E5", "S1 E6", "S1 E7", "S1 E8", "S1 E9", "S1 E10", "S1 E11", "S1 E12", "S1 E13",
     "Zesshoushinai 1",
@@ -32,29 +32,35 @@ var episodes = [
     "Zesshoushinai XV 1", "Zesshoushinai XV 2", "Zesshoushinai XV 3", "Zesshoushinai XV 4", "Concert XV"
 ]
 
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-    token: process.env["BOT_TOKEN"],
-    autorun: true
-});
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.includes('!whichepisode')) {
-        let today = new Date();
-        let dateToday = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-        let episode = episodes[diffDays(beginning, dateToday)];
+/*
+if (message.includes('!whichepisode')) {
+    let today = new Date();
+    let dateToday = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    let episode = episodes[diffDays(beginning, dateToday)];
+*/
 
-        bot.sendMessage({
-            to: channelID,
-            message: episode
-        });
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+token = process.env.BOT_TOKEN;
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+    if (msg.content.includes('whichepisode')) {
+        let dateToday = Date.now();
+        let nbEpisode = diffDays(beginning, dateToday);
+        let episode = episodes[nbEpisode];
+        msg.reply(episode);
+    }
+    if (msg.content.includes('whichtomorrow')) {
+        let dateToday = Date.now();
+        let nbEpisode = diffDays(beginning, dateToday);
+        let episode = episodes[nbEpisode+1];
+        msg.reply(episode);
     }
 });
-//Comment 2
+
+client.login(token);
