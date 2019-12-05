5807c9d2ce0680e0abe87455be05430f4443b638
Alexandre Viglione <alexxe33@gmail.com>
2019-12-05 15:42:09 +0100

    Fixed a mistake of mine

--- lib.js
+++ lib.js
@@ -46,12 +46,12 @@ const episodeDate = msg => {
 
     if (inputCmd === "Today") {
         theDate = new Date();
-        dateToday.setHours(0, 0, 0);
+        theDate.setHours(0, 0, 0);
     }
     if (inputCmd === "Tomorrow") {
         theDate = new Date();
-        dateTmr.setDate(dateTmr.getDate() + 1);
-        dateTmr.setHours(0, 0, 0);
+        theDate.setDate(theDate.getDate() + 1);
+        theDate.setHours(0, 0, 0);
     }
     if (inputCmd.includes("Date")) {
         const dayMonthYear = inputDate.split("/");
